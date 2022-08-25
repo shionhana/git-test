206f12aa894e49a495ea6963b7a6f0156cf12f64
Shion <Shion@qq.com>
2022-08-25 20:06:19 +0800

    third commit

--- src/main/java/com/git/Git.java
+++ src/main/java/com/git/Git.java
@@ -8,6 +8,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git1");
+        System.out.println("hello git2");
 
     }
 }
